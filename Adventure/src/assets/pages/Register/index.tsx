7f49ea5ac94e08448ac5d4aa92d097fc8d90b698
Marcos <wgngui14@gmail.com>
2023-11-27 19:26:42 -0300

    V 0.1

--- Adventure/src/assets/pages/Register/index.tsx
+++ Adventure/src/assets/pages/Register/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { FormRegister } from "./styles";
 
 
 export const Register: React.FC = () => {
     return(
         <div>
-            <h2>Register</h2>
+            <FormRegister>
+                <input type="text" name="" id="" placeholder="Nome"/>
+                <input type="date" name="" id="" placeholder="Data de nascimento"/>
+                <input type="email" name="" id="" placeholder="Digite novamente"/>
+                <input type="email" name="" id="" placeholder="E-mail"/>
+                <input type="text" name="" id="" placeholder="Senha"/>
+                <input type="text" name="" id="" placeholder="Digite novamente"/>
+                <input type="button" value="Registrar" />
+                
+            </FormRegister>
         </div>
     )
 }
---
+++ Adventure/src/assets/pages/Register/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const FormRegister = styled.form `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background-color: blue;
+    
+    &input {
+        margin-top: 10px;
+        margin: 100px;
+        height: 20%;
+        
+    }
+
+`
